9294419efc2c1a771085dfc5c277eadddfc4b9c7
linwe2012 <linwe2012@163.com>
2018-05-14 22:31:22 +0800

    new

--- NodeJs/assets/js/text.js
+++ NodeJs/assets/js/text.js
@@ -71,17 +71,36 @@
 (function(){
 	var dialog = document.querySelector('dialog');
     var showDialogButton = document.querySelector('#add');
+	var showDialogButton_small = document.querySelector('#add-small');
 	var submmit = document.querySelector('#submit-add');
+	var text_reset = document.querySelector('#reset-add');
+	var label = document.getElementById('icon-fab-add');
+	var label_small = document.getElementById('icon-fab-add-small');
+	var current_button;
+	var current_label;
+	
+	
 	
     if (! dialog.showModal) {	
       dialogPolyfill.registerDialog(dialog);
     }
     showDialogButton.addEventListener('click', function() {
 	  document.getElementById('label-diaglog-add').innerHTML='I am thinking...';
+	  current_button=showDialogButton;
+	  current_label=label;
+      dialog.showModal();
+    });
+	showDialogButton_small.addEventListener('click', function() {
+	  document.getElementById('label-diaglog-add').innerHTML='I am thinking...';
+	  current_button=showDialogButton_small;
+	  current_label=label_small;
       dialog.showModal();
     });
     dialog.querySelector('.close').addEventListener('click', function() {
       dialog.close();
+    });
+	text_reset.addEventListener('click', function() {
+	  document.getElementById('text-add').value='';
     });
 	var handler = function(event) {
 		document.getElementById('label-diaglog-add').innerHTML='I am thinking...';
@@ -100,9 +119,68 @@
 			snackbarContainer.MaterialSnackbar.showSnackbar(data);
 		}
 		else{
+			current_label.innerHTML = 'check';
+			current_button.style.backgroundColor='#ff4081';
+			setTimeout(function(){current_label.innerHTML = 'add';
+			current_button.style.backgroundColor='#ffeb3b'
+			},2000);
 			console.log(mytext);
 			dialog.close();
 		}
 	})
 }());
+
+
+(function(){
+	$('.my-simulate-tab').each(function(index,element){
+		var target = $(element).attr("name");
+		console.log('gotone type = ' + typeof element + '   '+ $(element));
+		$(element).click(function(){
+			var targets = target.split('#');
+			var i=1;
+			targets.splice(0,1);
+			console.log(targets);
+			for(var step of targets){
+				//setTimeout(function(){document.getElementById(step).click();console.log('timeout:'+i+step);}, 10*i);
+				//i+=10;
+				document.getElementById(step).click();
+			}
+			
+		})
+	})
+}())
+
+var i=11;
+document.querySelector('#progressbar1').addEventListener('mdl-componentupgraded', 
+    function() {
+    this.MaterialProgress.setProgress(11);
+});
+document.querySelector('#more1').addEventListener('click', 
+    function() {
+		i+=19;
+		if(i>=119){
+			i=0;
+		}
+		else if(i>100){
+			i=100;
+		}
+    document.querySelector('#progressbar1').MaterialProgress.setProgress(i);
+});
+
+(function(){
+	var ifsending=0;
+	var send = document.querySelector('#dandelion-send');
+	var sending = document.querySelector('#dandelion-sending')
+	send.addEventListener('click', 
+    function() {
+		console.log('click');
+		send.style.display='none';
+		sending.style.display='';
+		setTimeout(function(){
+				sending.style.display='none';
+				send.style.display='';
+			},2000)
+	});
+}() )
+
 
